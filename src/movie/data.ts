7aac0f022747e0cc14439740af190acac8538fd0
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2024-05-18 19:29:43 +0700

    feat: prevent empty array

--- src/movie/data.ts
+++ src/movie/data.ts
@@ -3,13 +3,13 @@ import { z } from 'zod'
 export const movieSchema = z.object({
   title: z.string().min(1),
   year: z.number().int().min(1900).max(2100),
-  genres: z.array(z.string().min(1)),
-  directors: z.array(z.string().min(1)),
-  writers: z.array(z.string().min(1)),
+  genres: z.array(z.string().min(1)).min(1),
+  directors: z.array(z.string().min(1)).min(1),
+  writers: z.array(z.string().min(1)).min(1),
   posterUrl: z.string().url(),
   type: z.enum(['movie', 'series']),
   plot: z.string().min(1),
-  actors: z.array(z.string().min(1)),
+  actors: z.array(z.string().min(1)).min(1),
 })
 
 type Movie = z.infer<typeof movieSchema> & { id: number }
