e5439d9f26a603d066f0b3160fb04a83f571a8fc
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2024-06-12 19:23:54 +0700

    feat: add postgres ssl mode configuration to env

--- src/db/db.ts
+++ src/db/db.ts
@@ -8,4 +8,5 @@ export const db = drizzle(postgres({
   database: process.env.DB_DATABASE,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
+  ssl: (process.env.DB_SSL as unknown) || false,
 }), { schema })
